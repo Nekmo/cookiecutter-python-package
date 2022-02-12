187672aa6ff8ad6055f6c06464071e70678566fb
Nekmo <contacto@nekmo.com>
2022-02-12 02:44:00 +0100

    Issue #4: Use setup.cfg for parameters

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/management.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/management.py
@@ -8,3 +8,15 @@ import click
 # @click.argument('<arg>')
 def manage():
     pass
+
+
+# Uncomment it to use subcommands:
+#
+# @click.group()
+# @click.option('--debug/--no-debug', default=False)
+# def cli(debug):
+#     click.echo(f"Debug mode is {'on' if debug else 'off'}")
+#
+# @cli.command()  # @cli, not @click!
+# def sync():
+#     click.echo('Syncing')
